0a574cc75d961d88bbad6f3b1c26e1f707247ce6
hilmicihan <se.hilmiyildirim@gmail.com>
2023-03-03 14:30:44 +0300

    add load testing

--- load_test.py
+++ load_test.py
@@ -1,25 +1,28 @@
 from locust import HttpUser, between, task
-
+# Setting a wait time of between 5 and 15 seconds for each user action
 class MyUser(HttpUser):
-    # Setting a wait time of between two numbers
-    wait_time=  between(5,15)
+    wait_time = between(5, 15)
 
+    # Defining a task to get all posts from the API
     @task
     def get_posts(self):
         self.client.get("/posts")
 
+    # Defining a task to get all users from the API
     @task
     def get_users(self):
         self.client.get("/users")
 
-    # Defining a task to create new post on the API
+    # Defining a task to create a new post on the API
     @task
     def create_post(self):
-        # Defininf the data to be posted
+        # Defining the data to be posted
         data = {
-            "title": "NEW POST",
+            "title": "New Post",
             "body": "This is a new post",
             "userId": 1
         }
+        # Defining the headers to be sent with the post request
         headers = {"Content-Type": "application/json"}
-        self.client.post("/posts", json= data, headers=headers)
+        # Sending a post request to create a new post with the specified data and headers
+        self.client.post("/posts", json=data, headers=headers)
